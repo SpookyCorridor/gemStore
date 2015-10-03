6f2c162420e2b7d27cf8ff7bedfd4a772f9f4c9a
Dylan <invision33@gmail.com>
2015-10-03 15:36:25 -0500

    level 4 complete - cleaning up directives and controllers

--- scripts/app.js
+++ scripts/app.js
@@ -6,15 +6,7 @@
 	});
 
 	app.controller('PanelController', function(){
-		this.tab = 1; 
-
-		this.selectTab = function(setTab) {
-			this.tab = setTab;
-		};
-
-		this.isSet = function(checkTab) {
-			return this.tab === checkTab; 
-		}
+		
 
 	});
 
@@ -28,6 +20,50 @@
 		};
 
 	});
+
+	app.directive('productTitle', function() {
+		return {
+			/*Use element directives for UI widgets and Attribute Directives 
+				for mixin behaviors like tooltips */
+			restrict: "E", //E for element 
+			templateUrl: "product-title.html"
+		};
+	});
+
+	app.directive("productSpecs", function() {
+    return { 
+    	restrict: "A",
+      templateUrl: "product-specs.html" 
+    };
+  }); 
+
+  app.directive("productReviews", function() {
+  	return {
+  		restrict: "E", 
+  		templateUrl: "product-reviews.html"
+  	};
+  });
+
+  app.directive("productPanels", function() {
+  	return {
+  		restrict: "E",
+  		templateUrl: "product-panels.html",
+  		controller: function() {
+
+  			this.tab = 1; 
+
+				this.selectTab = function(setTab) {
+					this.tab = setTab;
+				};
+
+				this.isSet = function(checkTab) {
+					return this.tab === checkTab; 
+				}; 
+
+  		},
+  		controllerAs: "panel"
+  	};
+  }); 
 
 })(); 
 
